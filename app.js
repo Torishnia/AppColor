0469fe11775dfa8bd12eee06d6a387e42a383919
Torishniia <valeri.torishnya@gmail.com>
2022-12-01 16:33:18 +0200

    Generate random color

---
+++ app.js
@@ -0,0 +1,34 @@
+const cols = document.querySelectorAll('.col');
+
+
+/*function generateRandomColor() {
+    const hexCodes = '0123456789ABCDEF';
+
+    let color = '';
+    for (let i = 0; i < 6; i++) {
+        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+    }
+    return '#' + color
+}*/
+
+function setRandomColors() {
+    cols.forEach((col) => {
+        const text = col.querySelector('h2');
+        const button = col.querySelector('button');
+        const color = chroma.random();  // generateRandomColor();
+
+        text.textContent = color;
+        col.style.background = color;
+
+        setTextColor(text, color);
+        setTextColor(button, color)
+    })
+}
+
+function setTextColor(text, color) {
+    const luminance = chroma(color).luminance();
+
+    text.style.color = luminance > 0.5 ? 'black' : 'white';
+}
+
+setRandomColors()
