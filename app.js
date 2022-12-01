6351bae6a1bded411812661ceda90caefacd2bdf
Torishniia <valeri.torishnya@gmail.com>
2022-12-01 17:05:36 +0200

    Change 'fa-lock-open'/'fa-lock'

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 const cols = document.querySelectorAll('.col');
 
+document.addEventListener('keydown', event => {
+    event.preventDefault();
+    if (event.code.toLowerCase() === 'space') {
+        setRandomColors()
+    }
+})
+
+document.addEventListener('click', event => {
+    const type = event.target.dataset.type;
+    
+    if (type === 'lock') {
+        const node = 
+        event.target.tagName.toLowerCase() === 'i'
+        ? event.target
+        : event.target.children[0];
+
+        node.classList.toggle('fa-lock-open');
+        node.classList.toggle('fa-lock');
+    }
+})
 
 /*function generateRandomColor() {
     const hexCodes = '0123456789ABCDEF';
@@ -13,9 +33,14 @@ const cols = document.querySelectorAll('.col');
 
 function setRandomColors() {
     cols.forEach((col) => {
+        const isLocked = col.querySelector('i').classList.contains('fa-lock');
         const text = col.querySelector('h2');
         const button = col.querySelector('button');
         const color = chroma.random();  // generateRandomColor();
+
+        if (isLocked) {
+            return
+        }
 
         text.textContent = color;
         col.style.background = color;
